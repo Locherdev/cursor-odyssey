0f1cdcdbb60fd1e6be982a472f26fc3840407343
Ton That Ngoc <nt023@hdm-stuttgart.de>
2020-06-29 19:05:44 +0200

    debris will now rotate randomly when instanced

--- game/src/debris/Debris.gd
+++ game/src/debris/Debris.gd
@@ -1,6 +1,11 @@
 extends KinematicBody2D
 
+var rotation_speed = float(rand_range(-2,2))
+
 func _on_Area2D_body_entered(body: Node) -> void:
 	if(body.name == 'Spaceship'):
 		Globals.append_to_statuslog("- Spaceship crashed into debris.")
 		queue_free()
+
+func _process(delta: float) -> void:
+	rotate(rotation_speed*delta)
