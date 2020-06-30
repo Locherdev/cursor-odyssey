15c52b3cd82229c1df1bf950687404969454ae67
Ton That Ngoc <nt023@hdm-stuttgart.de>
2020-06-30 11:10:05 +0200

    MusicLibrary restructured, more performance!

--- game/src/autoloads/MusicLibrary.gd
+++ game/src/autoloads/MusicLibrary.gd
@@ -1,74 +1,69 @@
 extends Node
 
-var current_track = []
-
-var track00 = {
+var tracks = [
+	{
 	"id": 0,
 	"name": "Nightsky",
 	"path": "res://assets/bgm/karakuri-night.ogg"
-	}
-
-var track01 = {
+	},{
 	"id": 1,
 	"name": "Rider of the Galaxy",
 	"path": "res://assets/bgm/citron.ogg"
-	}
-
-var track02 = {
+	},{
 	"id": 2,
 	"name": "Galactic Menace",
 	"path": "res://assets/bgm/bbjam.ogg" 
-	}
-
-var track03 = {
+	},{
 	"id": 3,
-	"name": "Strolling without an Aim",
+	"name": "Strolling Without an Aim",
 	"path": "res://assets/bgm/kimidori-vinyl.ogg"
-	}
-
-var track04 = {
+	},{
 	"id": 4,
-	"name": "From behind the Asteroid",
+	"name": "From Behind the Asteroid",
 	"path": "res://assets/bgm/mataro.ogg"
-	}
-
-var track05 = {
+	},{
 	"id": 5,
 	"name": "Courageous Dive",
 	"path": "res://assets/bgm/mintchoco.ogg"
-	}
-
-var track06 = {
+	},{
 	"id": 6,
 	"name": "Intergalactic Space Force",
 	"path": "res://assets/bgm/mu.ogg"
-	}
-
-var track07 = {
+	},{
 	"id": 7,
 	"name": "Hopelessly Lost",
 	"path": "res://assets/bgm/fuchsia.ogg"
+	},{
+	"id": 8,
+	"name": "Shooting Stars",
+	"path": "res://assets/bgm/melolyn.ogg"
+	},{
+	"id": 9,
+	"name": "In the Hopes of Meeting You",
+	"path": "res://assets/bgm/lemondays.ogg"
+	},{
+	"id": 10,
+	"name": "Scared of the Unknown",
+	"path": "res://assets/bgm/murasaki.ogg"
+	},{
+	"id": 11,
+	"name": "Passing the Slow Days",
+	"path": "res://assets/bgm/marbles.ogg"
+	},{
+	"id": 12,
+	"name": "My Human Tendencies",
+	"path": "res://assets/bgm/unit241.ogg"
 	}
-	
-var tracks = []
+]
 
-func _ready() -> void:
-	tracks.append(track00)
-	tracks.append(track01)
-	tracks.append(track02)
-	tracks.append(track03)
-	tracks.append(track04)
-	tracks.append(track05)
-	tracks.append(track06)
-	tracks.append(track07)
+var current_track = []
 
-func set_current_track(index):
+func set_current_track(index) -> String:
 	current_track = tracks[index]
 	return current_track.path
 
 func list_of_tracks(exclude_id) -> Array:
 	var list = []
 	for track in tracks:
-		if track.id != exclude_id:
-			list.append(track.id)
+		if track.id != exclude_id: list.append(track.id)
 	return list
