ce59be7f5faeb89bcc059013dc2d5f22f1d3a9f5
Ton That Ngoc <nt023@hdm-stuttgart.de>
2020-08-06 17:50:06 +0200

    titlescreen adjusted for more functionalities

--- game/src/userinterface/TitleUI.gd
+++ game/src/userinterface/TitleUI.gd
@@ -1,10 +1,13 @@
 extends Control
 
 func _ready() -> void:
+	
 	if $Easy.connect("pressed", self, "_on_Button_pressed", [0]) != OK: Globals.error_connect(self.name)
 	if $Normal.connect("pressed", self, "_on_Button_pressed", [1]) != OK: Globals.error_connect(self.name)
 	if $Hard.connect("pressed", self, "_on_Button_pressed", [2]) != OK: Globals.error_connect(self.name)
 	if $Highscores.connect("pressed", self, "_on_Button_pressed", [3]) != OK: Globals.error_connect(self.name)
+	if $Jukebox.connect("pressed", self, "_on_Button_pressed", [4]) != OK: Globals.error_connect(self.name)
+	if $Settings.connect("pressed", self, "_on_Button_pressed", [5]) != OK: Globals.error_connect(self.name)
 	var file2Check = File.new()
 	if !file2Check.file_exists(Highscores.highscores_path): Highscores.write_highscores()
 
@@ -14,3 +17,5 @@ func _on_Button_pressed(button_id):
 			Globals.difficulty = button_id
 			var _success = get_tree().change_scene("res://src/level/Level.tscn")
 		3: Globals.open_highscores()
+		4: print("Jukebox")
+		5: Globals.open_pause()
